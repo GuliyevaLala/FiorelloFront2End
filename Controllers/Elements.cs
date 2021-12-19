51f0a9e01d3e2367d2e4e409241eaf04c237cf13
GuliyevaLala <lalaig@code.edu.az>
2021-12-20 05:26:56 +0530

    first

---
+++ Controllers/Elements.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace weebbtasskkk.Controllers
+{
+    public class Elements : Controller
+    {
+        public ActionResult Progressbar()
+        {
+            return View();
+        }
+
+        public ActionResult Tabs()
+        {
+            return View();
+        }
+        public ActionResult Accordions()
+        {
+            return View();
+        }
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+    }
+}
+
